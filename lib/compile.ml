422df41ee42e1c133a1ea878fa5bd17460dfaf3f
Doug Woos <doug_woos@brown.edu>
2021-02-24 19:31:51 -0500

    Extend asm, fix input issue

--- lib/compile.ml
+++ lib/compile.ml
@@ -150,7 +150,7 @@ let compile_and_run (program : string) : string =
   ignore (Unix.system "nasm program.s -f macho64 -o program.o") ;
   ignore (Unix.system "gcc program.o runtime.o -o program") ;
   let inp = Unix.open_process_in "./program" in
-  let r = input_line inp in
+  let r = input_all inp in
   close_in inp ; r
 
 let compile_and_run_err (program : string) : string =
