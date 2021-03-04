3aa122c84a92e5d3496d7f432719d1b3ded536f7
Doug Woos <doug_woos@brown.edu>
2021-03-04 12:56:18 -0500

    Before class 3/4/2021

--- functions.lisp
+++ functions.lisp
@@ -19,3 +19,12 @@
 (define (even n) (if (zero? n) true (odd (sub1 n))))
 (define (odd n) (if (zero? n) false (even (sub1 n))))
 (print (even (read-num)))
+
+;; Example 5
+
+(define (fib-helper fib-n-2 fib-n-1 n target)
+  (let ((fib-n (+ fib-n-2 fib-n-1)))
+    (if (= n target) fib-n (fib-helper fib-n-1 fib-n (add1 n) target))))
+(define (fib n)
+  (if (< n 2) n (fib-helper 0 1 2 n)))
+(print (fib (read-num)))
